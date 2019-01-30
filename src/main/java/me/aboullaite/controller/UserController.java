84d7c879467581a91a96330fc28a0957e57ca15a
lingfeng.zhou <zhou.ling@husky.neu.edu>
2019-01-30 16:43:34 -0500

    update http status and json return type

--- src/main/java/me/aboullaite/controller/UserController.java
+++ src/main/java/me/aboullaite/controller/UserController.java
@@ -4,13 +4,13 @@ import me.aboullaite.model.User;
 import me.aboullaite.service.UserService;
 import me.aboullaite.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.Base64;
-import java.util.Date;
 
 @CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RestController
@@ -21,40 +21,56 @@ public class UserController {
 
 	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
 	@ResponseBody
-	public String register(@RequestBody User user) {
+	public StringResponse register(@RequestBody User user, HttpServletResponse response) {
 		User result = userService.findByEmail(user.getEmail());
 		if(result!=null){
-			return "Account already exists";
+			response.setStatus(HttpStatus.CONFLICT.value());
+			return new StringResponse("Account already exists");
 		}else{
-			String token = "Authorization: Basic " +
+			String token = "Authorization Token: " +
 					Base64Utils.encodeToString(
 							String.format("%s:%s", user.getEmail(), user.getPassword())
 									.getBytes()
 					);
 			user.setPassword(PasswordUtil.hashPassword(user.getPassword()));
 			userService.save(user);
-			return token;
+			return new StringResponse(token);
 		}
 	}
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	@ResponseBody
-	public String get(@RequestHeader("Authorization") String auth) {
-		System.out.println(auth);
-		if ((auth != null) && (auth.length() > 6)) {
+	public StringResponse get(@RequestHeader("Authorization") String auth, HttpServletResponse response) {
+		if ((auth != null) && (auth.length() > 0)) {
             byte[] b = Base64.getDecoder().decode(auth);
             String[] info = new String(b).split(":");
             User user = userService.findByEmail(info[0]);
             if(user == null){
-                return "not found user";
+				response.setStatus(HttpStatus.BAD_REQUEST.value());
+                return new StringResponse("user not found");
 			}else if(!PasswordUtil.verifyPassword(info[1],user.getPassword())){
-                return String.format("password not correct: %s, %s", info[1], user.getPassword());
+				response.setStatus(HttpStatus.BAD_REQUEST.value());
+                String res = String.format("password not correct: %s, %s", info[1], user.getPassword());
+                return new StringResponse(res);
             }else{
                 LocalDateTime d = LocalDateTime.now();
-                return d.toString();
+                return new StringResponse(d.toString());
             }
 
         }
-		return "not logged in, no token";
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		return new StringResponse("not logged in, no token");
+	}
+	private class StringResponse {
+
+		private String response;
+
+		public StringResponse(String s) {
+			this.response = s;
+		}
+
+		public String getResponse() {
+			return response;
+		}
 	}
 }
